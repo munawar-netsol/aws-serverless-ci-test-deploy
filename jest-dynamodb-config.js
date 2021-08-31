15871cae5a2c85dd57aefb1cc22be26cf99973cb
unknown <DELL@Lap-khi67.netsolpk.com>
2021-08-31 21:51:48 +0500

    '14th'

---
+++ jest-dynamodb-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    tables: [
+        {
+            TableName: 'player-point-circleci',
+            KeySchema: [
+                {
+                    AttributeName: 'ID',
+                    KeyType: 'HASH',
+                },
+            ],
+            AttributeDefinitions: [
+                {
+                    AttributeName: 'ID',
+                    AttributeType: 'S',
+                },
+            ],
+            BillingMode: 'PAY_PER_REQUEST',
+        },
+    ],
+    basePort: 8000,
+};
